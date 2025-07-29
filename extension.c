f687b24c7fe96fc2efd166d02150331dcbb5a8c5
Punyashreeg <punyashree.g@s.amity.edu>
2025-07-29 12:23:22 +0530

    a4

--- extension.c
+++ extension.c
@@ -5,3 +5,7 @@ int main() {
     printf("Enter an integer: ");
     scanf("%d", &number);
      if (number % 2 == 0)
+	        printf("%d is Even.\n", number);
+    else
+        printf("%d is Odd.\n", number);
+
