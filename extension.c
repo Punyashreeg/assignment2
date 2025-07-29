f9c33ec5e7b8898b214216173813576812c09c89
Punyashreeg <punyashree.g@s.amity.edu>
2025-07-29 12:19:47 +0530

    a1

--- extension.c
+++ extension.c
@@ -1,5 +1,6 @@
-int main() {
-	 int number;
+    int main() {
+         int number;
+
 
     
     printf("Enter an integer: ");
@@ -16,4 +17,4 @@ punyashree.g
 amity university
 b.tech cse
 Established in 2023, Amity Bengaluru is approved by UGC, AICTE, PCI, and BCI, with degrees recognized across India and abroad
-rowdy nanu
+rowdyu
